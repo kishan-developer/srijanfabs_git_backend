201273ab6cb8869bfdc1d990fc368d90d21e12f9
Krishnarajbhar767 <bloggerkrishna767@gmail.com>
2025-05-30 11:14:14 +0530

    Added SOme POpulate

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: "http://192.168.1.4:5173",
         credentials: true,
     })
 );
